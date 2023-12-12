553d2e7e547f16f0bd8a180b280d7ee938870568
alvcodec <148978672+alvcodec@users.noreply.github.com>
2023-12-12 09:17:04 +0000

    Adding new features - Timer

--- script.js
+++ script.js
@@ -2,30 +2,40 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
-const current = new Date();
+const dayOfTheWeek = document.getElementById("dayOfTheWeek");
+const numberDate = document.getElementById("numberDate");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function updateClock(){
+function updateClock() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let currentDay 
+    let d = new Date();
+    let fullDate = d.toLocaleDateString();
+    let day = days[d.getDay()]; // Returns the weekday of a date as a number (0-6). Filtered through the days array
     let ampm = "AM";
 
-    if(h > 12){
+    if (h > 12) {
         h = h - 12;
         ampm = "PM";
     }
     // if h is less than 10 then write "0" + h otherwise write h
-    h = h<10 ? "0" + h : h;
-    m = m<10 ? "0" + m : m;
-    s = s<10 ? "0" + s : s;
-
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
     ampmEl.innerText = ampm;
-    setTimeout(()=>{
+    dayOfTheWeek.innerText = day + " " + fullDate;
+ 
+    // dayOfTheWeek.innerText = day; 
+    // numberDate.innerText = fullDate;
+
+
+
+    setTimeout(() => {
         updateClock()
     }, 1000)
 }
